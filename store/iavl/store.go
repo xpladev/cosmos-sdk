2bded0891ca59ad032fd10a8145cba6e40072a27
Joowon Yun <40225835+JoowonYun@users.noreply.github.com>
2023-12-01 21:38:03 +0900

    Patch IAVL + fast export (#1)
    Co-authored-by: Reece Williams <reecepbcups@gmail.com>

--- store/iavl/store.go
+++ store/iavl/store.go
@@ -111,7 +111,7 @@ func UnsafeNewStore(tree *iavl.MutableTree) *Store {
 // Any mutable operations executed will result in a panic.
 func (st *Store) GetImmutable(version int64) (*Store, error) {
 	if !st.VersionExists(version) {
-		return nil, fmt.Errorf("version mismatch on immutable IAVL tree; version does not exist. Version has either been pruned, or is for a future block height")
+		return &Store{tree: &immutableTree{&iavl.ImmutableTree{}}}, nil
 	}
 
 	iTree, err := st.tree.GetImmutable(version)
